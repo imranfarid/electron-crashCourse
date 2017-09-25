20c5da8dbf1c93c2965e9a9c9e5cecbbc603ec2e
imranfarid <imran.cyborg@gmail.com>
2017-09-26 03:59:29 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,79 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+let addWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    //Create new window
+    mainWindow = new BrowserWindow({});
+    //Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    //Quit app when closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert Menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+//Handle create add window
+function createAddWindow(){
+    //Create new window
+    addWindow = new BrowserWindow({
+        width:300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    //Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    //Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+}
+
+//Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label:'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label:'Clear Items'
+            },
+            {
+                label:'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            },
+        ]
+    }
+];
+
+//If mac, add empty object to menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
